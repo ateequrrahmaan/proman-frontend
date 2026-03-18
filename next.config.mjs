e9852952bb16a54e58857d614ea96314c0747222
ateeq-dar <dynamicapplicationsai@gmail.com>
2026-03-18 20:47:56 +0530

    face-1 of deployeemnt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://projectmanage-backend-h3cj.onrender.com/api'
+  }
+};
 
 export default nextConfig;
